0a6cff4efaf4ff5181d1ed5d6358a3028804a633
wolf.dylan@redshred.com <dylanw3@umbc.edu>
2025-05-06 17:39:20 -0400

    Fixed debug paint bug

--- packages/flutter_box_transform/lib/src/transformable_box.dart
+++ packages/flutter_box_transform/lib/src/transformable_box.dart
@@ -870,7 +870,7 @@ class _TransformableBoxState extends State<TransformableBox> {
                           onHandleRotateEnd(event, handle),
                       onRotationCancel: () => onHandleRotateCancel(handle),
                       builder: widget.cornerHandleBuilder,
-                      debugPaintHandleBounds: kDebugMode,
+                      debugPaintHandleBounds: widget.debugPaintHandleBounds,
                     ),
                 if (widget.resizable)
                   for (final handle in HandlePosition.sides.where((handle) =>
@@ -891,31 +891,32 @@ class _TransformableBoxState extends State<TransformableBox> {
                       onPanEnd: (event) => onHandlePanEnd(event, handle),
                       onPanCancel: () => onHandlePanCancel(handle),
                       builder: widget.sideHandleBuilder,
-                      debugPaintHandleBounds: kDebugMode,
+                      debugPaintHandleBounds: widget.debugPaintHandleBounds,
                     ),
               ],
             ),
           ),
         ),
-        Positioned.fromRect(
-          rect: unrotatedRect,
-          child: IgnorePointer(
-            child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.green, width: 2)),
+        if(widget.debugPaintHandleBounds && kDebugMode)
+          Positioned.fromRect(
+            rect: unrotatedRect,
+            child: IgnorePointer(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.green, width: 2)),
+              ),
             ),
           ),
-        ),
-
-        Positioned.fromRect(
-          rect: boundingRect,
-          child: IgnorePointer(
-            child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.red, width: 3)),
+        if(widget.debugPaintHandleBounds && kDebugMode)
+          Positioned.fromRect(
+            rect: boundingRect,
+            child: IgnorePointer(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.red, width: 3)),
+              ),
             ),
           ),
-        ),
       ],
     );
   }
